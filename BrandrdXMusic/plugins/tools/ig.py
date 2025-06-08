dcdb5ef18417dce73e6ae67a28524586eebb35a0
bairagi2oo3 <tusharbairage95@gmail.com>
2025-06-08 21:53:12 +0530

    Update ig.py

--- BrandrdXMusic/plugins/tools/ig.py
+++ BrandrdXMusic/plugins/tools/ig.py
@@ -28,8 +28,8 @@ async def download_instagram_video(client, message):
         await a.edit("Fᴀɪʟᴇᴅ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ ʀᴇᴇʟ")
 
 
-__MODULE__ = "Iɴsᴛᴀɢʀᴀᴍ"
+__MODULE__ = "<blockquote><b>Iɴsᴛᴀɢʀᴀᴍ"
 __HELP__ = """/reel [ɪɴsᴛᴀɢʀᴀᴍ ʀᴇᴇʟ ᴜʀʟ] - Tᴏ ᴅᴏᴡɴʟᴏᴀᴅ ᴛʜᴇ ʀᴇᴇʟ ʙʏ ʙᴏᴛ
 /ig [ɪɴsᴛᴀɢʀᴀᴍ ʀᴇᴇʟ ᴜʀʟ] - Tᴏ ᴅᴏᴡɴʟᴏᴀᴅ ᴛʜᴇ ʀᴇᴇʟ ʙʏ ʙᴏᴛ
-/instagram [ɪɴsᴛᴀɢʀᴀᴍ ʀᴇᴇʟ ᴜʀʟ] - Tᴏ ᴅᴏᴡɴʟᴏᴀᴅ ᴛʜᴇ ʀᴇᴇʟ ʙʏ ʙᴏᴛ
+/instagram [ɪɴsᴛᴀɢʀᴀᴍ ʀᴇᴇʟ ᴜʀʟ] - Tᴏ ᴅᴏᴡɴʟᴏᴀᴅ ᴛʜᴇ ʀᴇᴇʟ ʙʏ ʙᴏᴛ</b></blockquote>
 """
