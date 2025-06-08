907a870325954a051c33bf6d82b7281561c872cb
bairagi2oo3 <tusharbairage95@gmail.com>
2025-06-08 21:12:00 +0530

    Update extras.py

--- BrandrdXMusic/utils/inline/extras.py
+++ BrandrdXMusic/utils/inline/extras.py
@@ -18,7 +18,7 @@ def close_markup(_):
         [
             [
                  InlineKeyboardButton(
-                text="🥀 ꜱᴜᴘᴘᴏʀᴛ 🥀", url=f"https://t.me/BRANDED_WORLD"
+                text="•⏤⁠⁠⁠⁠⛦⃕͜ 𝐒ᴜᴘᴘᴏʀᴛ", url=f"https://t.me/KING_BOT_UPDATE"
             ),
                 InlineKeyboardButton(
                     text=_["CLOSE_BUTTON"],
