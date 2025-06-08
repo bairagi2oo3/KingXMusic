07a2ac2d5dc08136b1a915665dc50d5ea17a3c9b
bairagi2oo3 <tusharbairage95@gmail.com>
2025-06-08 21:43:24 +0530

    Update vctools.py

--- BrandrdXMusic/plugins/tools/vctools.py
+++ BrandrdXMusic/plugins/tools/vctools.py
@@ -31,7 +31,7 @@ async def brah2(_, msg):
 # invite members on vc
 @app.on_message(filters.video_chat_members_invited)
 async def brah3(app: app, message: Message):
-    text = f"<blockquote><b>➻ {message.from_user.mention}\n\n๏ ɪɴᴠɪᴛɪɴɢ ɪɴ ᴠᴄ ᴛᴏ :\n\n➻ "
+    text = f"➻ {message.from_user.mention}\n\n๏ ɪɴᴠɪᴛɪɴɢ ɪɴ ᴠᴄ ᴛᴏ :\n\n➻ "
     x = 0
     for user in message.video_chat_members_invited.users:
         try:
@@ -43,13 +43,13 @@ async def brah3(app: app, message: Message):
     try:
         invite_link = await app.export_chat_invite_link(message.chat.id)
         add_link = f"https://t.me/{app.username}?startgroup=true"
-        reply_text = f"{text} 🤭🤭</b></blockquote>"
+        reply_text = f"<blockquote><b>{text} 🤭🤭</b></blockquote>"
 
         await message.reply(
             reply_text,
             reply_markup=InlineKeyboardMarkup(
                 [
-                    [InlineKeyboardButton(text="๏ ᴊᴏɪɴ ᴠᴄ ๏", url=add_link)],
+                    [InlineKeyboardButton(text="😘𝐉ᴏɪɴ 𝐕ᴄ 🤪", url=add_link)],
                 ]
             ),
         )
