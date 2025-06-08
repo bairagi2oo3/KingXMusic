31b81f2b0df9330ca1f6a34868543a0a632d5ec2
bairagi2oo3 <tusharbairage95@gmail.com>
2025-06-08 20:48:54 +0530

    Update song.py

--- BrandrdXMusic/utils/inline/song.py
+++ BrandrdXMusic/utils/inline/song.py
@@ -15,7 +15,7 @@ def song_markup(_, vidid):
         ],
         [
             InlineKeyboardButton(
-                text="🥀 sᴜᴩᴩᴏʀᴛ 🥀", url=f"{config.SUPPORT_CHAT}",
+                text="•⏤⁠⁠⁠⁠⛦⃕͜ 𝐒ᴜᴘᴘᴏʀᴛ", url=f"{config.SUPPORT_CHAT}",
             ),
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"], callback_data="close"
